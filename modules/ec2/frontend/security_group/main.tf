71a51a81413883e169e9fbc8d6412ce92f2b89a6
Rafaqwert <rafael.rrodrigues@sptech.school>
2024-12-02 19:54:05 -0300

    feat(): permite trafego ssh para ec2

--- modules/ec2/frontend/security_group/main.tf
+++ modules/ec2/frontend/security_group/main.tf
@@ -8,6 +8,14 @@ resource "aws_security_group" "sg_frontend" {
   }
 
   ingress {
+    description = "Allow SSH inbound traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "Allow HTTP inbound traffic"
     from_port   = 80
     to_port     = 80
